41a8d2b036d75c9e9cdc88d11e535d9fe36a80b2
kumarpritam1468 <manoharipritam@gmail.com>
2024-04-11 18:24:27 +0530

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import Header from "./components/Header/Header"
+import List from "./components/List/List"
+import Map from "./components/Map/Map"
+import PlaceDetails from "./components/PlaceDetails/PlaceDetails"
+
+import { Grid, CssBaseline } from '@material-ui/core'
+
 function App() {
 
   return (
     <>
-      Hello
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{width:'100%'}}  >
+        <Grid item xs={12} md={4} >
+          <List/>
+        </Grid>
+        <Grid item xs={12} md={4} >
+          <Map/>
+        </Grid>
+      </Grid>
     </>
   )
 }
