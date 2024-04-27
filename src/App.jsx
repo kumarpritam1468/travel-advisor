01c102d48062a07b3d54be5df54d8887f0005dbb
Pritam <manoharipritam@gmail.com>
2024-04-27 18:00:59 +0530

    add dynamic api

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react"
 import Header from "./components/Header/Header"
 import List from "./components/List/List"
 import Map from "./components/Map/Map"
-import PlaceDetails from "./components/PlaceDetails/PlaceDetails"
 
 import { getPlacesData } from './api'
 
@@ -14,7 +13,9 @@ function App() {
   const [places, setPlaces] = useState([]);
   const [coordinates, setCoordinates] = useState({});
   const [bounds, setBounds] = useState({});
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
@@ -24,14 +25,14 @@ function App() {
 
   useEffect(() => {
     setIsLoading(true);
-    getPlacesData(bounds.sw, bounds.ne).then((data) => {
+    getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
       // console.log(data);
       setPlaces(data);
       setIsLoading(false);
     }).catch((err) => {
       console.error(err);
     })
-  }, [coordinates, bounds]);
+  }, [coordinates, bounds, type]);
 
 
   // AIzaSyAi5hFSUpw-_Ev7RHeu7AQQ5xWyyrPO8EE
@@ -42,7 +43,7 @@ function App() {
       <Header />
       <Grid container spacing={3} style={{ width: '100vw' }}  >
         <Grid item xs={12} md={4} >
-          <List places={places} childClicked={childClicked} isLoading={isLoading} />
+          <List places={places} childClicked={childClicked} isLoading={isLoading} type={type} setType={setType} rating={rating} setRating={setRating} />
         </Grid>
         <Grid item xs={12} md={4} >
           <Map setCoordinates={setCoordinates} setBounds={setBounds} coordinates={coordinates} places={places} setChildClicked={setChildClicked} />
